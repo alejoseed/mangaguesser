04703ab29441f642e424778d5557fbf571c00d76
Alejandro <alejandrojpalmar@hotmail.com>
2023-02-13 22:22:49 -0500

    xx

--- src/components/play.tsx
+++ src/components/play.tsx
@@ -38,7 +38,7 @@ export interface playColors {
   colorThree: string
   colorFour: string
 }
-let corsProxy = "https://delicate-wave-6197.fly.dev/";
+let corsProxy = "https://sparkling-wildflower-3888.fly.dev/";
 
 let ImageParams = {
   correctNum: 0,
@@ -70,13 +70,13 @@ const CallAPI = async () => {
   for (let i = 0; i < 4; i++) {
     if (i !== correctNum) {
       apiCalls.push(
-        axios.get(mangadex, { params,
+        axios.get(mangadex, {
+          params: params,
           headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-            }
-        })
+              'X-Requested-With': 'application/xml'
+          }
+          }
+          )
           .then((res) => {
             mangaNames[i] = res.data.data.attributes.title.en;
           })
@@ -86,13 +86,13 @@ const CallAPI = async () => {
       );
     } else {
       apiCalls.push(
-        axios.get(mangadex, { params,
+        axios.get(mangadex, {
+          params: params,
           headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-            }
-          })
+              'X-Requested-With': 'application/xml'
+          }
+          }
+          )
           .then((res) => {
             mangaNames[correctNum] = res.data.data.attributes.title.en;
             ImageParams.mangaId = res.data.data.id;
@@ -129,14 +129,12 @@ function HandleAnswer(answer : number, manga : number) {
 
 async function GetImage() {
   try {
-    const res1 = await axios.get(ImageParams.baseUrl + ImageParams.mangaId + "/feed", {
+    const res1 = await axios.get(corsProxy + ImageParams.baseUrl + ImageParams.mangaId + "/feed", {
       headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-        }
-    });
-
+          'X-Requested-With': 'application/xml'
+      }
+      }
+      );
     if (res1.data.data.length === 0) {
       // change the correct answer to the next manga
       correctNum = randomNumber();
@@ -149,14 +147,12 @@ async function GetImage() {
     ImageParams.chapterId = res1.data.data[Math.floor(Math.random() * res1.data.data.length)].id;
     console.log(ImageParams.chapterId);
 
-    const res2 = await axios.get(ImageParams.atHome + ImageParams.chapterId, {
+    const res2 = await axios.get(corsProxy + ImageParams.atHome + ImageParams.chapterId, {
       headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-      }
-    });
-
+          'X-Requested-With': 'application/xml'
+          }
+        });
+    
     ImageParams.imageUrl += res2.data.chapter.hash + "/" + res2.data.chapter.dataSaver[Math.floor(Math.random() * res2.data.chapter.dataSaver.length)];
     console.log(ImageParams.imageUrl);
   } catch (error) {
