3847f8d5872d5638cde96f77eb28ff54ba48a516
alejoseed <alejandrojpalmar@hotmail.com>
2023-04-09 16:52:29 -0400

    Fixed landing page

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -8,10 +8,10 @@ function Body() {
     let textKey = useRef(0);
     const navigate = useNavigate();
     return(
-        <div className='grid justify-items-center roll-out'>
+        <div className='grid justify-items-center roll-out mb-3'>
             <h2 key={Math.random()} className="homeText roll-out">TEST YOUR MANGA SKILLS LIKE NEVER BEFORE</h2>
             <img src="https://firebasestorage.googleapis.com/v0/b/mangaguesser.appspot.com/o/narutosasuke.png?alt=media&token=ec9dbd36-7db6-426d-9249-57ef43f23cc9" alt="mangaguesser"
-            className="rounded-xl shadow-xl max-w-[320px] md:max-w-[480px] pt-6" />
+            className="rounded-xl shadow-xl max-w-[230px] md:max-w-[480px] pt-6" />
             <div className='pt-2'>
                 <div className="flex flex-col space-x-4 items-center justify-center roll-out">
                     <button className="align bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full justify-center" onClick={(e) => navigate("/play")}>Play Now!</button>
