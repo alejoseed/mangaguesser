7a6eaa30f9660d10cc33088518bf5d72ab184bdc
alejoseed <alejandrojpalmar@hotmail.com>
2023-04-09 16:12:49 -0400

    Changes to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import './index.css'
 import NavBar from './components/navBar'
 import React from 'react';
 import Body from './components/body'
-import {
-  useQuery,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
 
 function App() {
   return (
